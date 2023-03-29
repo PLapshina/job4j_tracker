1b06dc37640a72e0d2b6fd52269afc0e6dbd9c6e
Polina Lapshina <110300326+PLapshina@users.noreply.github.com>
2023-03-29 16:16:27 +0300

    1.3. Вызов метода с аргументами. [#166086]

---
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,20 @@
+package ru.job4j.oop;
+
+public class Jukebox {
+    public void music(int position) {
+        String lyrics = "Песня не найдена";
+        if (position == 1) {
+            lyrics = "Пусть бегут неуклюже";
+        } else if (position == 2) {
+            lyrics = "Спокойной ночи";
+        }
+        System.out.println(lyrics);
+    }
+
+    public static void main(String[] args) {
+        Jukebox jukebox = new Jukebox();
+        jukebox.music(1);
+        jukebox.music(2);
+        jukebox.music(5);
+    }
+}
