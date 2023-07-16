a06045c1fa1f17201ebd5e56979a0663e786b681
Polina Lapshina <110300326+PLapshina@users.noreply.github.com>
2023-07-16 13:42:52 +0300

    0. Set, HashSet, Iterator. [#10095 #430104] исправление

--- src/main/java/ru/job4j/collection/UsageHashSet.java
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -8,8 +8,10 @@ public class UsageHashSet {
         autos.add("Lada");
         autos.add("BMW");
         autos.add("Volvo");
+        autos.add("Volvo");
         autos.add("Toyota");
-        for (String auto :autos) {
+        autos.add("BMW");
+        for (String auto : autos) {
             System.out.println(auto);
         }
     }
