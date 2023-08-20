65a12786d5102e5638387c13ba3b468d3e592849
Polina Lapshina <110300326+PLapshina@users.noreply.github.com>
2023-08-20 19:40:37 +0300

    3. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,34 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.LinkedHashSet;
+import java.util.Collections;
+
+public class Departments {
+    public static List<String> fillGaps(List<String> deps) {
+        Set<String> tmp = new LinkedHashSet<>();
+        for (String value : deps) {
+            String str = "";
+            for (String el : value.split("/")) {
+                if ("".equals(str)) {
+                    str = el;
+                } else {
+                    str = str + "/" + el;
+                }
+                tmp.add(str);
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        Collections.sort(orgs);
+    }
+
+    public static void sortDesc(List<String> orgs) {
+        Collections.sort(orgs,  new DepDescComp());
+
+    }
+}
